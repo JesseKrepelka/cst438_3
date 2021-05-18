4a98689f834a1d77f1d79016627313a84d0564b3
JesseKrepelka <jessekrepelka@gmail.com>
2021-05-18 14:37:23 -0700

    Initial commit

---
+++ cst438_3/src/main/java/cst438_3/Cst4383Application.java
@@ -0,0 +1,13 @@
+package cst438_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4383Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst4383Application.class, args);
+	}
+
+}
---
+++ cst438_3/src/test/java/cst438_3/Cst4383ApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst4383ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
